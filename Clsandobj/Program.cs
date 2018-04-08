6c5b5ea0bcb227bf9153005c8d65de57867f26cf
Makoto Tanabe <xekid78@outlook.com>
2018-04-08 11:18:00 +0900

    Add files via upload

---
+++ Clsandobj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Clsandobj
+{
+    class Message
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    class Clsandobj
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+            msg.HelloWorld();
+        }
+    }
+}
